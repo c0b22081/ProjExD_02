12480ad66f2310f59272f70fd2a7668233af03e4
c0b22081 <c0b2208126@edu.teu.ac.jp>
2023-10-03 15:22:47 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -53,6 +53,9 @@ def main():
             if event.type == pg.QUIT: 
                 return
             
+        if kk_rct.colliderect(bd_rct):
+            return    
+            
         screen.blit(bg_img, [0, 0])
 
         """こうかとん"""
